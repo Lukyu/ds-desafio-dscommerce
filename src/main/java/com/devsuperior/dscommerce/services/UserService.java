01d867ab5b9ba1dfa081202046527dde0a244860
Lukyu <lutamber@gmail.com>
2023-10-18 22:06:46 -0300

    Adding user roles to UserService return

--- src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -1,13 +1,17 @@
 package com.devsuperior.dscommerce.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dscommerce.entities.Role;
 import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.exceptions.ResourceNotFoundException;
+import com.devsuperior.dscommerce.projections.UserDetailsProjection;
 import com.devsuperior.dscommerce.repositories.UserRepository;
 
 @Service
@@ -18,10 +22,18 @@ public class UserService implements UserDetailsService {
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User user = userRepository.findByEmail(username);
+
+		List<UserDetailsProjection> userRoles = userRepository.searchUserAnRolesByEmail(username);
 		
-		if(user == null)
+		if(userRoles.size() == 0)
 			throw new ResourceNotFoundException("User not found.");
+		
+		User user = new User();
+		user.setEmail(username);
+		user.setPassword(userRoles.get(0).getPassword());
+		
+		for(UserDetailsProjection p : userRoles)
+			user.addRole(new Role(p.getRoleId(), p.getAuthority()));
 		
 		return user;
 	}
