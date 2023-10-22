ff17cddaf9a7f6283958c90814134acb8902dfdb
Lukyu <lutamber@gmail.com>
2023-10-22 20:04:29 -0300

    Product pageable controller

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,12 +1,32 @@
 package com.devsuperior.dscommerce.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
+import com.devsuperior.dscommerce.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
+	@Autowired
+	private ProductService productService;
 	
+	@GetMapping
+	public ResponseEntity<Page<ProductMinDTO>> findAll(
+			@RequestParam(name = "name", defaultValue = "") String name,
+			Pageable pageable) {
+		
+		Page<ProductMinDTO> dto = productService.findAll(name, pageable);
+		return ResponseEntity.ok(dto);
+		
+	}
 	
 }
